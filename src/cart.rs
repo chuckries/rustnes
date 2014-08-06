64f468bbaffc6da003a62ec008c4c6ee31579361
Chuck Ries <chuck.ries@gmail.com>
2014-08-06 09:23:37 -0700

    playing around with vectors of bytes arrays
    I will eventually need to read in the bytes from the rom. The cart
    object is for the purpose of holding every memory bank, which ideally
    will be hidden behind mem, so the view from the nes is that there are
    only two program banks

--- src/cart.rs
+++ src/cart.rs
@@ -1,9 +1,19 @@
+use types::{byte};
+
 pub struct Cart {
-   name: String
+    name: String,
+
+    rom_data: Vec<[byte, ..0x100]>
 }
 
 impl Cart {
   pub fn new() -> Cart {
-      Cart{ name: String::from_str("Hello") }
+      let mut data: Vec<[byte, ..0x100]> = Vec::new();
+      data.push([0, ..0x100]);
+
+      Cart{ 
+          name: String::from_str("Hello"),
+          rom_data: data
+      }
   }
 }
