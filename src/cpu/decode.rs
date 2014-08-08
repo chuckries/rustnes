16a5ea6913578a9e70273fa97f69e11f5323dd89
Chuck Ries <chuck.ries@gmail.com>
2014-08-08 10:03:12 -0700

    started cpu run

--- src/cpu/decode.rs
+++ src/cpu/decode.rs
@@ -1,5 +1,54 @@
 pub struct Instruction(Instr, AddressMode);
 
+impl Instruction {
+    pub fn new(opcode: u8) -> Option<Instruction> {
+        decode(opcode)
+    }
+}
+
+fn decode(opcode: u8) -> Option<Instruction> {
+    let (instr, mode) = decode_impl(opcode);
+
+    match (instr, mode) {
+        (INSTR_NONE, ADDRESS_MODE_NONE) => None,
+        (x, y) => Some(Instruction(x,y))
+    }
+}
+
+//so this macro doesn't actually work, but leave it in for now
+//this could be used to expand out alu instructions easily in the match statement
+macro_rules! alu_inst(
+    ($instr:ident $imm:ident $zp:ident $zpx:ident $abs:ident $absx:ident $absy:ident $indx:ident $indy:ident) => (
+        $imm => ($instr, IMM),
+        $zp => ($instr, ZP),
+        $zpx => ($instr, ZPX),
+        $abs => ($instr, ABS),
+        $absx => ($instr, ABSX),
+        $absy => ($instr, ABSY),
+        $indx => ($instr, INDX),
+        $indy => ($instr, INDY),
+    );
+)
+
+fn decode_impl(opcode: u8) -> (Instr, AddressMode)
+{
+    match opcode {
+        0x00 => (BRK, IMP),
+
+        0x69 => (ADC, IMM),
+        0x65 => (ADC, ZP),
+        0x75 => (ADC, ZPX),
+        0x6D => (ADC, ABS),
+        0x7D => (ADC, ABSX),
+        0x79 => (ADC, ABSY),
+        0x61 => (ADC, INDX),
+        0x71 => (ADC, INDY),
+
+        _ => (INSTR_NONE, ADDRESS_MODE_NONE)
+    }
+
+}
+
 pub enum Instr {
     ADC,
     AND,
@@ -63,52 +112,18 @@ pub enum Instr {
 
 pub enum AddressMode {
     ZP,     //Zero Page             AND $12
-    IZP,    //Indexed ZeroPage      AND $12,X       LDX $12,Y
+    ZPX,    //Indexed ZeroPage X    AND $12,X       
+    ZPY,    //Indexed ZeroPage Y    LDX $12,Y
     ABS,    //Asolute               AND $1234
-    IABS,   //Indexed Absolute      AND $1234,X     AND $1234,Y
+    ABSX,   //Indexed Absolute X    AND $1234,X     
+    ABSY,   //Indexed Absolute Y    AND $1234,Y
     IND,    //Indirect              JMP ($1234)
-    IMP,    //Implied               CLD             NOP
-    ACC,    //Accumulator           ASL             ROL
+    IMP,    //Implied               CLD
+    ACC,    //Accumulator           ASL
     IMM,    //Immediate             AND #$12
     REL,    //Relative              BCS *+5
-    IIND,   //Indexed Indirect      AND ($12,X)
-    INDI,   //Indirect Indexed      AND ($12),Y
+    INDX,   //Indexed Indirect      AND ($12,X)
+    INDY,   //Indirect Indexed      AND ($12),Y
 
     ADDRESS_MODE_NONE
-}
-
-pub fn decode(opcode: u8) -> Option<Instruction> {
-    let (instr, mode) = decode_impl(opcode);
-
-    match (instr, mode) {
-        (INSTR_NONE, ADDRESS_MODE_NONE) => None,
-        (x, y) => Some(Instruction(x,y))
-    }
-}
-
-//so this macro doesn't actually work, but leave it in for now
-//this could be used to expand out alu instructions easily in the match statement
-macro_rules! alu_inst(
-    ($instr:ident $imm:ident $zp:ident $zpx:ident $abs:ident $absx:ident $absy:ident $indx:ident $indy:ident) => (
-        $imm => ($instr, IMM),
-        $zp => ($instr, ZP),
-        $zpx => ($instr, ZPX),
-        $abs => ($instr, ABS),
-        $absx => ($instr, ABSX),
-        $absy => ($instr, ABSY),
-        $indx => ($instr, INDX),
-        $indy => ($instr, INDY),
-    );
-)
-
-fn decode_impl(opcode: u8) -> (Instr, AddressMode)
-{
-    match opcode {
-        0x00 => (BRK, IMP),
-
-        //opcode
-
-        _ => (INSTR_NONE, ADDRESS_MODE_NONE)
-    }
-
 }
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,19 +1,45 @@
 use mem;
+use self::decode::Instruction;
 
 mod decode;
 
 struct ProcState {
     //processor registers
-    PC: u16,    //Program Counter
-    A:  u8,     //Accumulator
-    X:  u8,     //Index Register X
-    Y:  u8,     //Index Register Y
-    SP: u8,     //Stack Pointer
-    P:  u8,     //Status Register
+    pub PC: u16,    //Program Counter
+    pub A:  u8,     //Accumulator
+    pub X:  u8,     //Index Register X
+    pub Y:  u8,     //Index Register Y
+    pub SP: u8,     //Stack Pointer
+    pub P:  u8,     //Status Register
 }
 
+pub struct Cpu {
+    state: ProcState,
+}
 
-pub fn run() {
-    mem::read();
-    mem::write();
+impl Cpu {
+    pub fn new() -> Cpu {
+        let proc_state = ProcState {
+            PC: 0x000,
+            A:  0x00,
+            X:  0x00,
+            Y:  0x00,
+            SP: 0x00,
+            P:  0x00,
+        };
+
+        Cpu { state: proc_state }
+    }
+
+    pub fn run(&mut self) {
+        let &mut state = &self.state;
+        let opcode = read(state.PC);
+        let instr = Instruction::new(opcode);
+    }
+}
+
+//simple prototype so I can write around a reader
+//not permament
+fn read(addr: u16) -> u8 {
+    0x00
 }
