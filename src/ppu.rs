136270140ad23a66e153f5be8d7891743e07c723
Chuck Ries <chuck.ries@gmail.com>
2014-08-18 21:50:02 -0700

    add some documentation on the ppu registers

--- src/ppu.rs
+++ src/ppu.rs
@@ -34,3 +34,81 @@
 /// |_ _ _ _ _ _ _ _ _ _| $1000 | Pattern Tables |
 /// | Pattern Table 0   |       |                |
 /// |___________________| $0000 |________________|
+///
+///
+/// # PPU Registers
+///
+/// This is from http://nesdev.com/NESDoc.pdf
+///
+/// $2000 - PPU Control Register 1 - Write Only
+///
+/// - Bits 1-0 -  Name Table address, changes between the four
+///   name tables at $2000 (0b00), $2400 (0b01), $2800 (0b10) and $2C00 (0b11).
+///
+/// - Bit 2 - Specifies the amout to increment address by, either 1 if 
+///   this is 0 or 32 if this is 1.
+///
+/// - Bit 3 - Identifies which pattern table Sprites are stored in,
+///   either $0000 (0) or $1000 (1).
+///
+/// - Bit 4 - Identifies which pattern table the background 
+///   is store in, either $0000 (0) or $1000 (1).
+///
+/// - Bit 5 - Specifies the size of sprites in pixels, 8x8 
+///   if this is 0, 8x16 if this is 1.
+///
+/// - Bit 6 - Changes PPU between master and slave modes. 
+///   This is not used by the NES.
+///
+/// - Bit 7 - Indicates whether a NMI should occur up V-Blank.
+///
+/// $2001 - PPU Control Register 2 - Write Only
+///
+/// - Bit 0 - Indicates whether the system is in color (0) or 
+///   monochrome mode (1).
+///
+/// - Bit 1 - Specifies whether to clip the background, that is whether
+///   to hide the background in the left 8 pixels on screen (0) or 
+///   or to show them (1).
+///
+/// - Bit 2 - Specifies whether to clip the sprites, that is whether
+///   to hide sprites in the left 8 pixels on screen (0) or 
+///   or to show them (1).
+///
+/// - Bit 3 - If this is 0, the background should not be displayed
+///
+/// - Bit 4 - If this is 0, sprites should not be dispalyed
+///
+/// - Bits 7-5 - Indicates background color in monochrome mode or 
+///   color intensity in color mode.
+///
+/// $2002 - PPU Status Register - Read Only
+///
+/// - Bit 4 - If set, indicates that writes to VRAM should be ignored.
+///
+/// - Bit 5 - Scanline sprite count, if set, indicates more than 8 
+///   sprites on the current scanline.
+///
+/// - Bit 6 - Sprite 0 hit flag, set when a non-transparent pixel of
+///   sprite 0 overlaps a non-transparent background pixel
+///
+/// - Bit 7 - Indicates whether V-Blank is occurring.
+///
+/// $2003 - SprRam VAddr Register - Write Only
+///
+/// - Holds the VAddr in SprRam to access on the next write to $2004
+///
+/// $2004 - SprRam I/O Register - Write Only
+///
+/// - Writes a byte to SprRam at the VAddr indicated by $2003
+///
+/// $2005 - VRAM Address Register 1 - Write Only
+///
+/// $2006 - VRAM Address Register 2 - Write Only
+///
+/// $2007 - VRAM I/O Register - Read/Write
+///
+/// - Reads or writes a byte from VRAM at the current address.
+///
+/// TODO
+/// DMA Register ($4014) and Joypad I/O Registers ($4016 and $4017)
